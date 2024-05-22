72193747fc1ca9015cf7af4f0e81885548edf410
Luke Selby <lukeselby3@gmail.com>
2024-05-22 15:39:06 -0500

    Delete is functional

--- pdf_tools.py
+++ pdf_tools.py
@@ -9,28 +9,6 @@ from pathlib import Path
 import queue
 import PyPDF2
 
-# Function to be called when the button is clicked
-def del_on_button_click() -> None:
-    # Show an "Open" dialog box and return the path to the selected file
-    pdf_path_string = askopenfilename()
-
-    # Create a Path object from the string
-    pdf_path = Path(pdf_path_string)
-
-    # Open the PDF file with open_pdf function and create a reader object
-    pdf_file = open_pdf(pdf_path)
-
-    # If pdf file exists, continue
-    if pdf_file != None:
-        # Create a reader object from the file
-        pdf_file_reader_in = PyPDF2.PdfReader(pdf_file)
-
-        # Call Delete pages function
-        delete_pages(pdf_file_reader_in)
-
-        # Close the file
-        pdf_file.close()
-    
 # Function to open a PDF file  
 def open_pdf(pdf_path: Path):
     # Get the path to the file as a string
@@ -58,17 +36,6 @@ def open_pdf(pdf_path: Path):
 
     return pdf_file
 
-# Function for processing delete window input
-def process_delete_input(pages_to_delete_str: str) -> list:
-    pages_to_delete_list = pages_to_delete_str.split(",")
-
-    result = queue.Queue()
-    
-    for x in pages_to_delete_list:
-        result.put(int(x))
-
-    return result
-
 # Function for delete pages dialouge
 def delete_pages(pdf_in: PyPDF2.PdfFileReader):
 
@@ -80,36 +47,76 @@ def delete_pages(pdf_in: PyPDF2.PdfFileReader):
     # Open Options Dialouge
     pages_to_delete_str = simpledialog.askstring("Delete Pages", "Enter the pages to delete separated by commas, no spaces")
 
-    pages_to_delete_int = process_delete_input(pages_to_delete_str)
+    # If the user actually input something, continue
+    if pages_to_delete_str is not None and pages_to_delete_str != "":
 
-    page_num = 1
+        pages_to_delete_int = process_delete_input(pages_to_delete_str)
+
+        page_num = 1
     
-    for page in pdf_in_list:
+        for page in pdf_in_list:
 
-        # If pages_to_delete_int is empty, go ahead and add page
-        if pages_to_delete_int.empty():
-            print("Adding page " + str(page_num))
-            pdf_writer.add_page(page)
-
-
-        # If pages_to_delete_int is not empty, check the head to see if page should be added
-        else:
-            # Do not add page if it is in pages_to_delete_int
-            if page_num != pages_to_delete_int.queue[0]:
+            # If pages_to_delete_int is empty, go ahead and add page
+            if pages_to_delete_int.empty():
                 print("Adding page " + str(page_num))
                 pdf_writer.add_page(page)
-            # dequeue
+
+
+            # If pages_to_delete_int is not empty, check the head to see if page should be added
             else:
-                pages_to_delete_int.get()
+                # Do not add page if it is in pages_to_delete_int
+                if page_num != pages_to_delete_int.queue[0]:
+                    print("Adding page " + str(page_num))
+                    pdf_writer.add_page(page)
+                # dequeue
+                else:
+                    pages_to_delete_int.get()
         
-        # Increment page number
-        page_num += 1
+            # Increment page number
+            page_num += 1
                 
-    # save the new pdf file
-    with open('new_file.pdf', 'wb') as f:
-        pdf_writer.write(f)
+        # save the new pdf file
+        with open('new_file.pdf', 'wb') as f:
+            pdf_writer.write(f)
+
+
+# Function to be called when the button is clicked
+def del_on_button_click() -> None:
+    # Show an "Open" dialog box and return the path to the selected file
+    pdf_path_string = askopenfilename()
+
+    # Create a Path object from the string
+    pdf_path = Path(pdf_path_string)
+
+    # Open the PDF file with open_pdf function and create a reader object
+    pdf_file = open_pdf(pdf_path)
+
+    # If pdf file exists, continue
+    if pdf_file != None:
+        # Create a reader object from the file
+        pdf_file_reader_in = PyPDF2.PdfReader(pdf_file)
+
+        # Call Delete pages function
+        delete_pages(pdf_file_reader_in)
+
+        # Close the file
+        pdf_file.close()
 
     
+
+# Function for processing delete window input
+def process_delete_input(pages_to_delete_str: str) -> list:
+    pages_to_delete_list = pages_to_delete_str.split(",")
+
+    result = queue.Queue()
+    
+    for x in pages_to_delete_list:
+        result.put(int(x))
+
+    
+
+    return result
+
 
 # Create the main window
 root = tk.Tk()
